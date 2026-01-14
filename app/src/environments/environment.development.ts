93c4757ca84ee7957d3c9f9c02341701cff77016
alexis Christaud-braize <alexis.christaud@gmail.com>
2026-01-14 16:08:58 +0100

    env

--- app/src/environments/environment.development.ts
+++ app/src/environments/environment.development.ts
@@ -5,12 +5,12 @@ export const environment = {
 };
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyD2VJz1RDD1XDeVC2L6dMmdcMXlpl9hlcY",
-  authDomain: "la-bonne-biere.firebaseapp.com",
-  projectId: "la-bonne-biere",
-  storageBucket: "la-bonne-biere.firebasestorage.app",
-  messagingSenderId: "722729387483",
-  appId: "1:722729387483:web:ffc06a7121b29351ad93fe",
-  measurementId: "G-6C6PT2LQZK"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 
 };
